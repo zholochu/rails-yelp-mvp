c9ca00bd9fa602324190b9b5de7c4a9f1826fb10
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2022-11-09 13:39:28 -0500

    Code update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian]
+
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5]
+
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: RATINGS }
 end
